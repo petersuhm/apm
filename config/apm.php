90b8fdf9a6bcab9fe7bca26bc74ba847db682a9f
vistik <hr.kloft@gmail.com>
2018-08-30 23:15:51 +0200

    more settings plus comments

--- config/apm.php
+++ config/apm.php
@@ -3,11 +3,27 @@
 return [
     // Filtering
 
-    // RequestedAtTimezone
-
     'timezone' => 'UTC',
-
     'trackFullScanQueries' => true,
 
-    'showBindings' => true,
+    /*
+      |--------------------------------------------------------------------------
+      | This will save the full sql's including values (password etc). Default: false
+      |--------------------------------------------------------------------------
+    */
+    'showBindings' => false,
+
+    /*
+      |--------------------------------------------------------------------------
+      | Log database queries to log file in addition to the database. Default: false
+      |--------------------------------------------------------------------------
+    */
+    'saveQueriesToLog' => false,
+
+    /*
+      |--------------------------------------------------------------------------
+      | Clean up database after x days (both request & queries)
+      |--------------------------------------------------------------------------
+    */
+    'keepLogFor' => 30,
 ];
