cf20e5606903b9974cc95de4fac58de95ac2fde1
vistik <hr.kloft@gmail.com>
2018-08-31 00:09:06 +0200

    can save request to logs as well

--- src/Jobs/StoreRequestData.php
+++ src/Jobs/StoreRequestData.php
@@ -2,6 +2,7 @@
 
 namespace Vistik\Apm\Jobs;
 
+use Illuminate\Support\Facades\Log;
 use Vistik\Apm\Request;
 use Vistik\Apm\Request\RequestResponseData;
 use Illuminate\Bus\Queueable;
@@ -35,6 +36,9 @@ class StoreRequestData implements ShouldQueue
      */
     public function handle()
     {
+        if (config('apm.saveRequestsToLog', false)){
+            Log::debug(sprintf("Logged request: %s [%s]: %s", $this->data->getStatusCode(), $this->data->getMethod(), $this->data->getUrl()));
+        }
         Request::create([
             'uuid'             => $this->data->getUuid(),
             'user_id'          => $this->data->getUserId(),
